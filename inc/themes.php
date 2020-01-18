4368fc0b147bebd95f7d67a04547d0ca97f57e64
djphil <djphil@users.noreply.github.com>
2020-01-18 15:30:12 +0100

    Indentation
    Indentation

--- inc/themes.php
+++ inc/themes.php
@@ -1,29 +1,29 @@
 <?php 
 function construire_url($dossier) 
 {
-	return 'css/' . htmlspecialchars($dossier) . '/' .htmlspecialchars($dossier). '.css'; 
+    return 'css/' . htmlspecialchars($dossier) . '/' .htmlspecialchars($dossier). '.css'; 
 }
 
 $dossiers = array (
     'default',
-	'amelia',
-	'cerulean',
-	'cosmo',
-	'cyborg',
-	'darkly',
-	'flatly',
-	'freelancer',
-	'journal',
-	'lumen',
-	'paper',
-	'readable',
-	'sandstone',
-	'simplex',
-	'slate',
-	'spacelab',
-	'superhero',
-	'united',
-	'yety'
+    'amelia',
+    'cerulean',
+    'cosmo',
+    'cyborg',
+    'darkly',
+    'flatly',
+    'freelancer',
+    'journal',
+    'lumen',
+    'paper',
+    'readable',
+    'sandstone',
+    'simplex',
+    'slate',
+    'spacelab',
+    'superhero',
+    'united',
+    'yety'
 );
 
 $new_style = (isset($_GET['style'])) ? $_GET['style'] : ''; 
@@ -31,13 +31,13 @@ $cookie_style = (isset($_COOKIE['style'])) ? $_COOKIE['style'] : '';
 
 if (in_array($new_style, $dossiers, true)) 
 {
-	setcookie('style', $new_style, time() + (365 * 24 * 3600), '/');
-	$url = construire_url($new_style); 
+    setcookie('style', $new_style, time() + (365 * 24 * 3600), '/');
+    $url = construire_url($new_style); 
 }
 
 else if (in_array($cookie_style, $dossiers, true))
 {
-	$url = construire_url($cookie_style);
+    $url = construire_url($cookie_style);
 }
 else {$url = construire_url($dossiers[0]);} 
 ?> 
